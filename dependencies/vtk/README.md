599b794fd1bd92a92694b83e0569e2657d4a13c5
mark14wu <mark14@qq.com>
2018-06-19 02:50:38 +0800

    modified folder structure of dependencies

---
+++ dependencies/vtk/README.md
@@ -0,0 +1,6 @@
+if cmake errors, like the following:
+	"X11_Xt_LIB could not be found.  Required for VTK X lib."
+
+please install the following libraries:
+
+sudo apt install libxt-dev
--- install.sh
+++ install.sh
@@ -7,10 +7,7 @@ git submodule init
 git submodule update
 mkdir build
 cd build
-mkdir itkv3
-cd itkv3
-cp ../../../../dependencies/itkKernelFunction.h .
-cd ..
+cp -r ../../../../dependencies/itkv3 .
 cmake .. -DCMAKE_INSTALL_PREFIX="/usr" -DCMAKE_CXX_FLAGS="-fPIC -I'itkv3' -DITKV3_COMPATIBILITY=1" -DBUILD_OUTSIDE_INSIGHT_APPLICATIONS=ON -DBUILD_TESTING=OFF -DFLTK_FLUID_EXECUTABLE="/usr/bin/fluid"
 make
 sudo make install
